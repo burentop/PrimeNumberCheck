1bdc6de41513e45101f9236550ed1e6a4c78e35c
Brent Perry <burentoperii@gmail.com>
2016-07-09 14:43:35 -0500

    Initial Commit

---
+++ PrimeNumberCheck/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PrimeNumberCheck
+//
+//  Created by Brent Perry on 7/9/16.
+//  Copyright © 2016 Brent Perry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
