8f804614b9275589ddd239dc886d502667147be3
Brent Perry <burentoperii@gmail.com>
2016-07-10 13:36:01 -0500

    Add functionality to allow input to set max number; also fix logic checking for prime.

--- PrimeNumberCheck/main.cpp
+++ PrimeNumberCheck/main.cpp
@@ -9,8 +9,11 @@
 #include "../../../std_lib_facilities.h"
 
 bool check_for_prime(int number) {
-    vector<int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+    vector<int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103};
     int i = 0;
+    if (number <= 1) {
+        return false;
+    }
     while (primes[i] < number) {
         if (number % primes[i] == 0) {
             return false;
@@ -23,14 +26,20 @@ bool check_for_prime(int number) {
 int main()
 {
     vector<int> found_primes;
+    int max;
     
-    for (int i = 0; i <=100; ++i) {
+    cout << "Please enter a whole number: ";
+    cin >> max;
+    cout << "\n";
+    
+    
+    for (int i = 1; i <=max; ++i) {
         if (check_for_prime(i)) {
             found_primes.push_back(i);
         }
     }
     
-    cout << "List of prime numbers found: \n";
+    cout << "List of prime numbers found between 1 and your number: \n";
     for (int num : found_primes) {
         cout << num << " ";
     }
