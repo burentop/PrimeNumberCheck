bf62f5ff2540c0d972a7f64d688a247a96b2bbf7
Brent Perry <burentoperii@gmail.com>
2016-07-10 13:27:28 -0500

    Initial commit of PrimeNumberCheck.

--- PrimeNumberCheck/main.cpp
+++ PrimeNumberCheck/main.cpp
@@ -6,10 +6,33 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+bool check_for_prime(int number) {
+    vector<int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+    int i = 0;
+    while (primes[i] < number) {
+        if (number % primes[i] == 0) {
+            return false;
+        }
+        ++i;
+    }
+    return true;
+}
+
+int main()
+{
+    vector<int> found_primes;
+    
+    for (int i = 0; i <=100; ++i) {
+        if (check_for_prime(i)) {
+            found_primes.push_back(i);
+        }
+    }
+    
+    cout << "List of prime numbers found: \n";
+    for (int num : found_primes) {
+        cout << num << " ";
+    }
+    cout << "\n";
 }
